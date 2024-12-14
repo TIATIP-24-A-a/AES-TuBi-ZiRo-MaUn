f6ff491491afc90f902d0682891013fdabe997eb
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2024-12-14 16:33:47 +0100

    test(key_expansion): word rotate with words

--- KeyExpansion_Test.py
+++ KeyExpansion_Test.py
@@ -10,5 +10,13 @@ class KeyExpansionTestCase(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
+    def test_rotate_word_with_words(self):
+        input = ['Hallo', 'ich', 'bin', 'ein', 'Test']
+        expected = ['ich', 'bin', 'ein', 'Test', 'Hallo']
+
+        result = rot_word(input)
+
+        self.assertEqual(expected, result)
+
 if __name__ == '__main__':
     unittest.main()
