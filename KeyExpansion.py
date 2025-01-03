d39f2cf6b0b648ec2fbc10d8dc37711a3dd3476c
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2025-01-03 20:57:19 +0100

    refactor(key_expansion): return bytes

--- KeyExpansion.py
+++ KeyExpansion.py
@@ -2,8 +2,15 @@
 def rot_word(word):
     return word[1:] + word[:1]
 
-def key_expansion(key):
-    round_keys = [key]
+
+def key_expansion(key: str) -> list[bytes]:
+    """
+    Erstellt anhand Key, weitere Keys für die Runden
+    AES 128 -> 11 Keys
+    :param key:  Initial Key für die Erweiterung
+    :return: Liste von Keys in bytes
+    """
+    round_keys: list[bytes] = [key.encode()]
 
     for i in range(10):
         round_key = round_keys[-1]
@@ -11,5 +18,3 @@ def key_expansion(key):
         round_keys.append(round_key)
 
     return round_keys
-
-
