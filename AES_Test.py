809a33ce1de21d5d0aafc5f481986b58c34c5835
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2025-01-03 10:53:42 +0100

    test: add split blocks must return 16 bytes blocks with pad bytes

--- AES_Test.py
+++ AES_Test.py
@@ -4,7 +4,7 @@ from AES import split_blocks
 
 
 class AesTestCase(unittest.TestCase):
-    def test_to_blocks_must_return_blocks(self):
+    def test_split_blocks_must_return_16_bytes_blocks(self):
         text = 'Hallo ich binnnnHallo ich binnnnHallo ich binnnnHallo ich binnnn'
         expected = [
             'Hallo ich binnnn',
@@ -12,7 +12,17 @@ class AesTestCase(unittest.TestCase):
             'Hallo ich binnnn',
             'Hallo ich binnnn',
         ]
+        result = split_blocks(text)
 
+        self.assertEqual(result, expected)
+
+    def test_split_blocks_must_return_16_bytes_blocks_with_pad_bytes(self):
+        text = 'Hallo ich binnnnHallo ich binnnn 123'
+        expected = [
+            'Hallo ich binnnn',
+            'Hallo ich binnnn',
+            ' 123            ',
+        ]
         result = split_blocks(text)
 
         self.assertEqual(result, expected)
