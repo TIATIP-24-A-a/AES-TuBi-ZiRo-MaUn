0c0c20a369a857b9ce46e54df94a6568cf726de0
Tuan Binh Tran <28620517+tuanbinhtran@users.noreply.github.com>
2025-01-03 20:57:44 +0100

    test(key_expansion): fix return bytes

--- KeyExpansion_Test.py
+++ KeyExpansion_Test.py
@@ -1,6 +1,6 @@
 import unittest
+
 from KeyExpansion import rot_word, key_expansion
-import numpy as np
 
 
 class KeyExpansionTestCase(unittest.TestCase):
@@ -21,33 +21,37 @@ class KeyExpansionTestCase(unittest.TestCase):
         self.assertEqual(expected, result)
 
     def test_key_expansion_return_11_keys(self):
-        input = 'Hallo ich bin ein Mensch'
+        key = 'Hallo ich bin ein Mensch'
         expected = 11
 
-        result = key_expansion(input)
+        result = key_expansion(key)
 
         self.assertEqual(expected, len(result))
 
     def test_key_expansion_expect_16bytes_key(self):
-        input = 'Hallo ich binnnn'
+        key = 'Hallo ich binnnn'
 
-        result = key_expansion(input)
+        result = key_expansion(key)
         for i in result:
             self.assertEqual(len(i), 16)
 
     def test_key_expansion_input_must_occur_first(self):
-        input = 'Hallo ich binnnn'
+        key = 'Hallo ich binnnn'
+        expected = key.encode()
 
-        result = key_expansion(input)
-        self.assertEqual(result [0], input)
+        result = key_expansion(key)
+
+        self.assertEqual(expected, result[0])
 
     def test_key_expansion_result_must_be_unique_for_all_elements(self):
-        input = 'Hallo ich binnnn'
+        key = 'Hallo ich binnnn'
+        expected = 11
 
-        result = key_expansion(input)
-        uniqueResult = set(result)
+        result = key_expansion(key)
+        unique_result_len = len(set(result))
 
-        self.assertEqual(len(uniqueResult), 11)
+        self.assertEqual(expected, unique_result_len)
+
 
 if __name__ == '__main__':
     unittest.main()
